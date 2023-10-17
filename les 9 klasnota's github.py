e014a283cc64e8bcaabb7d088de42d06fb4fca5e
raf73nel <raf.nelissen73@gmail.com>
2023-10-17 08:30:53 +0200

    eerste uitgave

---
+++ les 9 klasnota's github.py
@@ -0,0 +1,23 @@
+#github install git git
+
+#hoe python script inlezen in visual studio code van github?
+
+#git pull
+#git push
+
+miro vendiagram
+
+#https://github.com/raf73nel
+
+# git config --global user.email "raf.nelissen73@gmail.com"
+# git config --global user.name "raf73nel"
+
+
+
+
+
+
+
+
+
+
