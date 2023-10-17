0d2ddfc2d1fe99d58b82b598b118c157db258bb5
raf73nel <raf.nelissen73@gmail.com>
2023-10-17 17:17:55 +0200

    commit3

--- les 9 klasnota's github.py
+++ les 9 klasnota's github.py
@@ -9,11 +9,68 @@ miro vendiagram
 
 #https://github.com/raf73nel
 
+
+# open the command prompt
+#give 2 lines below  
+
 # git config --global user.email "raf.nelissen73@gmail.com"
 # git config --global user.name "raf73nel"
 
+
+
+# -----------------------------------------------------------------------------------------------------------
+# from VSC to Github =>
+
+# open folder in VSC
+
+# open github
+# create new repository
+# copy URL
+
+# in visual studio code
+
 #initialize repository ctrl shift P type init.....
-# right click push and pull
+#  give commit message (bvb eerste versie)
+#  click on V (vink, next to source control), click Yes
+# click on 3dot icon (next to source control)
+# select "push to"
+# add remote and paste the URL; then provide a remote name (bvb: raf)
+# select "push to
+# select URL in list
+# -----------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------
+# Publish to github without making the repository in github
+
+# open folder in VSC
+# add files
+#initialize repository ctrl shift P type init.....
+#  click on V (vink, next to source control), click Yes
+#  give commit message (bvb eerste versie)
+# ctrl shift P : Publish to GitHub
+# click to allow acces
+# copy authorization token 
+#  in VSC click (below on screen) on signing in to github.com , paste authorisation code
+# ctrl shift P : Publish to GitHub
+# choose public
+# sign in with your browser
+# authorize
+# -----------------------------------------------------------------------------------------------------------
+
+# publish new folder
+#initialize repository ctrl shift P type init.....
+#  click on V (vink, next to source control), click Yes
+#  give commit message (bvb eerste versie)
+# ctrl shift P : Publish to GitHub
+# -----------------------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------
+# Clone project from github
+# 
+# copy URL from github (on top or click on code(green) and copy URL)
+# ctrl shift P : git.clone, provide URL
+# select repository location
+# -----------------------------------------------------------------------------------------------------------
+
 
 
 
