32fb63ba325a5c58410dafc5e6492fccff426afe
raf73nel <raf.nelissen73@gmail.com>
2023-10-18 16:50:55 +0200

    commit4

--- les 9 klasnota's github.py
+++ les 9 klasnota's github.py
@@ -5,7 +5,7 @@
 #git pull
 #git push
 
-miro vendiagram
+# miro vendiagram
 
 #https://github.com/raf73nel
 
@@ -21,15 +21,14 @@ miro vendiagram
 # -----------------------------------------------------------------------------------------------------------
 # from VSC to Github =>
 
-# open folder in VSC
-
 # open github
 # create new repository
 # copy URL
 
 # in visual studio code
 
-#initialize repository ctrl shift P type init.....
+# open folder in VSC
+# initialize repository ctrl shift P type init.....
 #  give commit message (bvb eerste versie)
 #  click on V (vink, next to source control), click Yes
 # click on 3dot icon (next to source control)
@@ -37,39 +36,26 @@ miro vendiagram
 # add remote and paste the URL; then provide a remote name (bvb: raf)
 # select "push to
 # select URL in list
-# -----------------------------------------------------------------------------------------------------------
-# -----------------------------------------------------------------------------------------------------------
-# Publish to github without making the repository in github
-
-# open folder in VSC
-# add files
-#initialize repository ctrl shift P type init.....
-#  click on V (vink, next to source control), click Yes
-#  give commit message (bvb eerste versie)
-# ctrl shift P : Publish to GitHub
-# click to allow acces
-# copy authorization token 
-#  in VSC click (below on screen) on signing in to github.com , paste authorisation code
-# ctrl shift P : Publish to GitHub
-# choose public
-# sign in with your browser
-# authorize
-# -----------------------------------------------------------------------------------------------------------
-
-# publish new folder
-#initialize repository ctrl shift P type init.....
-#  click on V (vink, next to source control), click Yes
-#  give commit message (bvb eerste versie)
-# ctrl shift P : Publish to GitHub
-# -----------------------------------------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------------------------------------
 # Clone project from github
-# 
+
 # copy URL from github (on top or click on code(green) and copy URL)
 # ctrl shift P : git.clone, provide URL
 # select repository location
 # -----------------------------------------------------------------------------------------------------------
+
+# -----------------------------------------------------------------------------------------------------------
+# Publish to github without making the repository in github
+
+# open folder in VSC
+# add files or make new file in new folder
+# initialize repository ctrl shift P type init.....
+# choose folder (see step 1)
+# Go to source control
+# give commit message (bvb init) + ctrl enter
+# ctrl shift P : Publish to GitHub
+# click to allow acces
 
 
 
